0331277bec41ca4fed27028a4b69289b89d6cb0d
marquesmaycon <mayconmarquesh@gmail.com>
2024-07-16 19:30:16 -0300

    trying to push

--- src/pages/TripDetails/TripDetailsPage.tsx
+++ src/pages/TripDetails/TripDetailsPage.tsx
@@ -14,6 +14,7 @@ const TripDetailsPage = () => {
     <div className="max-w-6x px-6 py-10 space-y-8">
       <DestinationAndDateHeader />
 
+      
       <main className="flex gap-16 px-4">
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
