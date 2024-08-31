cbf5752ac1d065da70ecd500819e030602b44752
marquesmaycon <mayconmarquesh@gmail.com>
2024-08-31 00:34:53 -0300

    actvt date fix

--- src/pages/TripDetails/modals/ActivityModal.tsx
+++ src/pages/TripDetails/modals/ActivityModal.tsx
@@ -4,7 +4,6 @@ import { Calendar, Tag, X } from "lucide-react"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 
-
 import Button from "../../../components/Button"
 import { queryClient } from "../../../libs/tanStackQuery"
 import { activityDefaultValues, ActivitySchema, activitySchema } from "../../../validation/schemas"
@@ -24,66 +23,66 @@ const ActivityModal = ({ setIsActivityModalOpen, activityId }: ActivityModalProp
     resolver: zodResolver(activitySchema),
   })
 
-  const { mutateAsync: createActivity, isPending: isCreating} = useCreateActivity(tripId!)
+  const { mutateAsync: createActivity, isPending: isCreating } = useCreateActivity(tripId!)
   const { mutateAsync: editActivity, isPending: isEditing } = useEditActivity(tripId!)
-  
+
   useEffect(() => {
     if (!activityId) return
-    reset(currAcitivity)
+    reset({ ...currAcitivity, startsAt: currAcitivity?.startsAt.slice(0, 16) })
   }, [activityId])
 
   const currAcitivity = queryClient
     .getQueryData<ActivitiesByDay[]>(["activities", tripId])
     ?.find((activity) => activity.activities.find((activity) => activity.id === activityId))
     ?.activities.find((activity) => activity.id === activityId)
-  
-  const tripDetails = queryClient.getQueryData<Trip>(["trip", tripId])
 
+  const tripDetails = queryClient.getQueryData<Trip>(["trip", tripId])
+  console.log(currAcitivity?.startsAt.slice(0, 16))
   async function onSubmit(data: ActivitySchema) {
     activityId ? await editActivity({ id: activityId!, ...data }) : await createActivity(data)
     setIsActivityModalOpen(false)
   }
 
   return (
-    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-      <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+    <div className="fixed inset-0 flex items-center justify-center bg-black/60">
+      <div className="w-[640px] space-y-5 rounded-xl bg-zinc-900 px-6 py-5 shadow-shape">
         <div className="space-y-2">
-          <div className="flex justify-between items-center">
+          <div className="flex items-center justify-between">
             <h2 className="text-lg font-semibold">{activityId ? "Editar" : "Criar"} atividade</h2>
             <button type="button" onClick={() => setIsActivityModalOpen(false)}>
-              <X className="text-zinc-400 size-5" />
+              <X className="size-5 text-zinc-400" />
             </button>
           </div>
           <p className="text-small text-zinc-400">Todos convidados podem visualizar as atividades.</p>
         </div>
 
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
-          <div className="px-4 h-14 py-2.5 border border-zinc-800 bg-zinc-950 rounded-lg flex gap-2.5 items-center">
+          <div className="flex h-14 items-center gap-2.5 rounded-lg border border-zinc-800 bg-zinc-950 px-4 py-2.5">
             <Tag className="size-5 text-zinc-400" />
 
             <input
-              className="bg-transparent text-lg placeholder-zinc-400 flex-1 outline-none"
+              className="flex-1 bg-transparent text-lg placeholder-zinc-400 outline-none"
               placeholder="Qual a atividade?"
               {...register("name")}
             />
           </div>
 
-          <div className="px-4 flex-1 h-14 py-2.5 border border-zinc-800 bg-zinc-950 rounded-lg flex gap-2.5 items-center">
+          <div className="flex h-14 flex-1 items-center gap-2.5 rounded-lg border border-zinc-800 bg-zinc-950 px-4 py-2.5">
             <Calendar className="size-5 text-zinc-400" />
 
             <input
               type="datetime-local"
-              min={tripDetails?.startsAt.split(".")[0]}
-              max={tripDetails?.endsAt.split(".")[0]}
+              min={tripDetails?.startsAt.slice(0, 16)}
+              max={tripDetails?.endsAt.slice(0, 16)}
               placeholder="Data e horário da atividade"
-              className="bg-transparent text-lg placeholder-zinc-400 flex-1 outline-none"
+              className="flex-1 bg-transparent text-lg placeholder-zinc-400 outline-none"
               {...register("startsAt")}
             />
           </div>
 
           <div>
             {Object.entries(errors).map(([field, error]) => (
-              <p key={field} className="text-red-400 text-xs">
+              <p key={field} className="text-xs text-red-400">
                 {error?.message}
               </p>
             ))}
